ad5864104f77579817e3f59c5d28a2631d8a2708
Zain Imran <zainimran@google.com>
2023-08-07 20:20:53 +0800

    fix pip install vln + add deploy script for admob function
    Change-Id: I4b343f29bb0b1caf7664a73494aad3223e8ee4e5

--- admob/run.sh
+++ admob/run.sh
@@ -21,7 +21,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 
 # install dependencies
-pip3 install -r requirements.txt
+pip install --require-hashes -r requirements.txt
 
 # run main script
 python3 main.py --generate-token-only=true
