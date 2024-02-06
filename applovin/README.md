0adead87ad5cf8334e0de56830d92aeaa46ed035
Zain Imran <zainimran@google.com>
2024-02-06 16:49:30 +0800

    add applovin module + config setup for analytics and applovin
    Change-Id: Iadd278eb26d665cfc40297774a41f85873bbb727

---
+++ applovin/README.md
@@ -0,0 +1,8 @@
+# Steps to run the script locally for testing/debugging/experimental purposes
+First, add the API key string to the .env file. Then run the following terminal ccommands in this directory:
+- `$ source ./venv/bin/activate`
+- `$ pip install -r requirements.txt --require-hashes`
+- `$ python3 main.py`
+
+# To deploy the script to GCP
+`$ sh deploy.sh --project cloud-project-id`
---
+++ utils/bulk_deletion_across_tables.py
@@ -0,0 +1,30 @@
+from google.cloud import bigquery
+
+# Initialize a BigQuery client
+client = bigquery.Client(project="project-id")
+
+# Define your dataset and table name pattern
+dataset_id = "dataset-id"
+
+# Define the condition for row deletion
+deletion_condition = "COLUMN >= 'value'"  # Example: "date < '2021-01-01'"
+
+# List all tables in the dataset
+tables = client.list_tables(dataset_id)
+table_names = [table.table_id for table in tables]
+
+print(table_names)
+
+# Loop over tables and delete rows matching the condition
+for table_name in table_names:
+    query = f"""
+        DELETE FROM `{dataset_id}.{table_name}`
+        WHERE {deletion_condition}
+    """
+    query_job = client.query(query)  # Run the query
+    query_job.result()  # Wait for the job to complete
+
+    print(f"Rows deleted from table {table_name}.")
+
+# Complete
+print("Deletion complete.")
